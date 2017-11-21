93cabd67e576d52face1e24e57f5833abb00af0e
Sam Clark <P1518741X@LEC-ADMIN.dmu.ac.uk>
2017-11-21 11:39:46 +0000

    added js loop for json file

--- javascript/data.js
+++ javascript/data.js
@@ -2,23 +2,29 @@ let jsonString = `
 { "lecturers": [
   { 
     "firstName":"Dave" ,
-    "lastName":"Everitt"
+    "lastName":"Everitt" ,
+	"roomNumber":"1",
+	"img": "https://ezgif.com/image-to-datauri/ezgif-4-f74a2e0ee9.jpg"
   },
   {
     "firstName":"Fania",
-    "lastName":"Raczinski"
+    "lastName":"Raczinski",
+	"roomNumber":"2"
   },
   {
     "firstName":"Luke",
-    "lastName":"Attwood"
+    "lastName":"Attwood",
+	"roomNumber":"3"
   },
   {
     "firstName":"David",
-    "lastName":"Smallwood"
+    "lastName":"Smallwood",
+	"roomNumber":"4"
   },
   {
     "firstName":"Clinton",
-    "lastName":"Ingrams"
+    "lastName":"Ingrams",
+	"roomNumber":"5"
   }
 ]}
 `;
--- javascript/parse-data.js
+++ javascript/parse-data.js
@@ -9,8 +9,19 @@
     Rewrite this to make it *loop* through all the lecturers' names. 
     (Note the ES6 'template literal' syntax):
   */
-  let theData =  `${obj.lecturers[1].firstName} ${obj.lecturers[1].lastName}`;
-  
+ 
+	let theData = '';
+	var i;
+	for(i = 0; i < obj.lecturers.length; i++){
+	theData =  `${theData} <br> ${obj.lecturers[i].firstName} ${obj.lecturers[i].lastName} <br> Room Number: ${obj.lecturers[i].roomNumber} <br> ${obj.lecturers[i].img}`;
+	}
+/*
+let theData = "";
+for (let key in obj.lecturers){
+	theData += '${obj.lecturers(key).firstName}
+	${obj.lecturers(key).lastName};
+}
+*/
   // populates the chosen element's content with the data:
   demoData.innerHTML = theData; 
 
@@ -19,4 +30,6 @@
     Once you have looped through all the data, present the 
     results on the page, styled with CSS.
   */
+  
+  
 }());
